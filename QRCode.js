9a7ecb85ef36c8107589c3f90a677a54fc114b06
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2016-09-22 20:54:28 +0800

    add js file

---
+++ QRCode.js
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 16/9/22.
+ */
+var QRCodeModule = require('react-native').NativeModules.QRCode;
+export default class QRCode{
+    static openQRCodePage(type:number) {
+    }
+    static closeQRCodePage(){
+
+    }
+};
