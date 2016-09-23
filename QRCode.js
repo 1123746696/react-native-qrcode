5bb4cf0ced90679098a5f99c0cdbaf67e8aa9e2d
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2016-09-23 10:35:30 +0800

    add test function

--- QRCode.js
+++ QRCode.js
@@ -2,10 +2,20 @@
  * Created by user on 16/9/22.
  */
 var QRCodeModule = require('react-native').NativeModules.QRCode;
-export default class QRCode{
-    static openQRCodePage(type:number) {
-    }
-    static closeQRCodePage(){
-
+var didGetRQCodeSuccessSubscription
+var QRCode={
+    /***
+     * 打开二维码页面
+     * @param type 1:条形码 2:二维码 3:条形码和二维码
+     */
+    openQRCodePage(type:number) {
+        QRCodeModule.openQRCodePage(type);
+    },
+    closeQRCodePage():Promise<boolean>{
+        return QRCodeModule.closeQRCodePage();
+    },
+    didGetRQCodeSuccess(handler: Function){
+        didGetRQCodeSuccessSubscription = this.addEventListener(QRCodeModule.DidGetRQCodeSuccess, handler);
     }
 };
+module.exports = QRCode;
---
+++ app.js
@@ -0,0 +1,71 @@
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
+} from 'react-native';
+import QRCode from './QRCode'
+class RCTQRCode extends Component {
+    constructor (props) {
+
+        super (props);
+        this.state = {
+            code:'获取到的二维码',
+            type:0,
+        }
+    }
+    componentDidMount(){
+        // QRCode.didGetRQCodeSuccess(({code,type})=>{
+        //     console.log('code====,',code,type)
+        //     this.setState({
+        //         code:code,
+        //         type:type
+        //     })
+        //     QRCode.closeQRCodePage().then((isClosed)=>{
+        //
+        //     })
+        // })
+    }
+    openQRCode(){
+        QRCode.openQRCodePage(1);
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this.openQRCode.bind(this)}>
+                    <Text style={styles.welcome}>
+                        打开二维码
+                    </Text>
+                </TouchableOpacity>
+
+                <Text style={styles.instructions}>
+                    {this.state.code+'   '+this.state.type}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('RCTQRCode', () => RCTQRCode);
